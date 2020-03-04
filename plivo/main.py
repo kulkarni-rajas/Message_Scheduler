d9be5f65f79d0213668fb55e808670e6d6e0c713
Himanshu Shekhar <brad272@pop-os.localdomain>
2020-03-04 14:18:39 +0530

    fix plivo issues

--- plivo/main.py
+++ plivo/main.py
@@ -1,15 +1,16 @@
 import plivo
 import sys
 
-client = plivo.RestClient(auth_id='MAZMEWYJY4NZC2ZGYYM2', auth_token='ODc2MzM4MDFkZmY1MjEwOGMxZTU4ZDMxYzU1MmVl')
-# info = sys.argv[1]
+account_id = "your_auth_id_here"
+account_token = "your_auth_token_here"
+client = plivo.RestClient(auth_id=account_id , auth_token=account_token)
+info = sys.argv[1]
 for i in range(0,1):
     try:
         message_created = client.messages.create(
-            src='+17272631094',
-            dst='+919647641633',
-            text=message
-            # text=info
+            src='source_number_here',
+            dst='destination_number_here',
+            text=info
         )
     except:
         print("Invalid number")
